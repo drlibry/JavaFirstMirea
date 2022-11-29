3980cca3d2fa4facb9423a4d86c6139b3a4b14bd
drlibry <rybakova.dasha.03@gmail.com>
2022-11-29 09:43:32 +0300

    о

---
+++ src/ru/mirea/task5/shape/TestRandomShape.java
@@ -0,0 +1,10 @@
+package ru.mirea.task5.shape;
+
+public class TestRandomShape
+{
+    public static void main(String[] args)
+    {
+        Framez window = new Framez("Random Shapes");
+        window.DrawRandomShape();
+    }
+}
